3964cdfd84e4bba3c44bebcbd25edb8b695dae32
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2025-06-04 15:52:37 +0900

    feat: Add ToDateTime method in WindowsNativeHelper class
    - Add new method ToDateTime to WindowsNativeHelper class.

---
+++ Source/_70_ServiceImplements/Windows/WindowsNativeHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Filedini.ServiceImplements;
+
+internal static class WindowsNativeHelper
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static DateTime ToDateTime(this FILETIME time)
+    {
+        var fileTime = ((long)time.dwHighDateTime << 32) | (uint)time.dwLowDateTime;
+        return DateTime.FromFileTime(fileTime);
+    }
+}
