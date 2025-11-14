40f7cce526cb5f88ee2d9605700743555073a80b
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2025-11-14 21:39:22 +0900

    feat: Convert FILETIME to DateTime directly.
    - Adds a method to convert FILETIME to DateTime.
    - Removes helper class for converting FILETIME to DateTime.

---
+++ Source/_70_ServiceImplements/Windows/WindowsNativeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Filedini.ServiceImplements;
+
+internal static class WindowsNativeExtensions
+{
+    extension(FILETIME time)
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public DateTime ToDateTime()
+        {
+            var fileTime = ((long)time.dwHighDateTime << 32) | (uint)time.dwLowDateTime;
+            return DateTime.FromFileTime(fileTime);
+        }
+    }
+}
